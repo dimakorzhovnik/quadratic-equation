ae08bd339955810ab8b22ee19e778ff7910d58fc
dimakorzhovnik <korzhovnik.dima@gmail.com>
2018-02-22 01:40:20 +0300

    task 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let result = [];
+    
+    var temp = equation.replace(/\s/g, '').match(/[^\^]\d+|^\d+/g);
+
+    let a = temp[0],
+        b = temp[1],
+        c = temp[2];
+    
+    let D = Math.sqrt(b * b - 4 * a * c);
+
+    result[0]= Math.round(-b - D) / (2 * a);
+    result[1]= Math.round(-b + D) / (2 * a);
+
+    result.sort((left, right) => left - right);
+    
+    return result;
 }
